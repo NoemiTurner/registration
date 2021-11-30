d216ee90b2f2181de69bdd00517adc72a07a4f57
NoemiTurner <70876373+NoemiTurner@users.noreply.github.com>
2021-11-30 13:30:50 -0800

    Registration App

--- registration/urls.py
+++ registration/urls.py
@@ -20,3 +20,4 @@ urlpatterns = [
     path('regserve/', include('regserve.urls')),
     path('admin/', admin.site.urls),
 ]
+
--- regserve/tests.py
+++ regserve/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.test_client = Client()
+    def test_response(self):
+        response = self.test_client.get('/regserve/hello')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Hello world from django backend")
